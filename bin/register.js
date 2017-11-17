ae3a9d89b9f514ce4a42e9a25a693fc649813e5b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-17 20:06:44 +0100

    Check for an argument

--- bin/register.js
+++ bin/register.js
@@ -1,1 +1,8 @@
-console.log('register to the workshop server');
+var script_arguments = process.argv.slice(2);
+
+if (script_arguments.length != 1) {
+    console.log('usage: npm run register <team-name>');
+    process.exit(1);
+}
+
+console.log(script_arguments[0]);
