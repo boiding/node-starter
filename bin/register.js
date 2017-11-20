e6b3cdd9c17f87c5dd916e45f71797eef5e84cfb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-20 09:21:06 +0100

    Make it possible to unregister a team

--- bin/register.js
+++ bin/register.js
@@ -1,30 +1,76 @@
 var ip = require('ip');
 var request = require('request');
+var command_line_arguments = require('command-line-args');
 
-var script_arguments = process.argv.slice(2);
+var command_options_definitions = [
+    { name: 'unregister', alias: 'u', type: Boolean },
+    { name: 'name', type: String, defaultOption: true }
+];
 
-if (script_arguments.length != 1) {
-    console.log('usage: npm run register <team-name>');
+var command_options;
+try {
+    command_options = command_line_arguments(command_options_definitions);
+} catch(error) {
+    usage_and_quit('could not parse command line arguments');
+}
+
+if (!command_options.name) {
+    usage_and_quit('missing require option \'team-name\'');
+}
+
+if (!command_options.unregister) {
+    register(command_options);
+} else {
+    unregister(command_options);
+}
+
+function usage_and_quit(reason) {
+    console.log(reason);
+    console.log('usage: npm run register [--unregister] <team-name>');
     process.exit(1);
 }
 
-var data = {
-    "ip_address": ip.address(),
-    "name": script_arguments[0]
-};
+function register(command_options) {
+    var data = {
+        'ip_address': ip.address(),
+        'name': command_options.name
+    };
 
-request({
-    url: 'http://localhost:2643/register',
-    method: 'POST',
-    json: data
-}, function(error, response, body){
-    if (error) {
-        console.log('whoops');
-        return;
-    }
-    if (response && response.statusCode === 204) {
-        console.log('successfully registered \'%s\' at %s', data.name, data.ip_address);
-    } else {
-        console.log('could not register: \'%s\'', response.body.reason);
-    }
-});
+    request({
+        url: 'http://localhost:2643/register',
+        method: 'POST',
+        json: data
+    }, function(error, response, body){
+        if (error) {
+            console.log('whoops');
+            return;
+        }
+        if (response && response.statusCode === 204) {
+            console.log('successfully registered \'%s\' at %s', data.name, data.ip_address);
+        } else {
+            console.log('could not register: \'%s\'', response.body.reason);
+        }
+    });
+}
+
+function unregister(command_options) {
+    var data = {
+        "name": command_options.name
+    };
+
+    request({
+        url: 'http://localhost:2643/register',
+        method: 'DELETE',
+        json: data
+    }, function(error, response, body){
+        if (error) {
+            console.log('whoops');
+            return;
+        }
+        if (response && response.statusCode === 204) {
+            console.log('successfully unregistered \'%s\'', data.name);
+        } else {
+            console.log('could not unregister: \'%s\'', response.body.reason);
+        }
+    });
+}
